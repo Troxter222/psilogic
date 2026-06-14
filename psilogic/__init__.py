bec068a1652ae97a2b581251722441cde09c524f
Ali <troxtergrif@gmail.com>
2026-06-14 16:40:00 +0300

    chore: 0.4.0 packaging with benchmark and integrations extras

--- psilogic/__init__.py
+++ psilogic/__init__.py
@@ -4,24 +4,49 @@ from __future__ import annotations
 
 from importlib.metadata import version as _pkg_version
 
-from .convenience import PsiLogicGPT, PsiLogicNLP, PsiLogicViT
+from . import debug
+from ._chaos import get_chaos_metrics
+from ._version import __version__ as _fallback_version
+from .convenience import (
+    PsiLogicGPT,
+    PsiLogicNLP,
+    PsiLogicViT,
+    PsiLogicWhisper,
+    build_auto_optimizer,
+    infer_architecture,
+)
 from .optimizer import PsiLogic
-from .param_groups import nlp_param_groups
-from .presets import gpt_scratch_defaults, nlp_defaults, vision_defaults
+from .param_groups import gpt_param_groups, nlp_param_groups, vit_param_groups
+from .presets import (
+    glue_defaults,
+    gpt_scratch_defaults,
+    nlp_defaults,
+    vision_defaults,
+    whisper_defaults,
+)
 
 try:
     __version__ = _pkg_version("psilogic")
 except Exception:
-    __version__ = "0.3.2"
+    __version__ = _fallback_version
 
 __all__ = [
-    "PsiLogic",
+    "PsiLogic",  # includes PsiLogic.auto() classmethod
     "PsiLogicNLP",
     "PsiLogicGPT",
     "PsiLogicViT",
+    "PsiLogicWhisper",
     "nlp_param_groups",
+    "vit_param_groups",
+    "gpt_param_groups",
     "nlp_defaults",
     "vision_defaults",
     "gpt_scratch_defaults",
+    "whisper_defaults",
+    "glue_defaults",
+    "get_chaos_metrics",
+    "infer_architecture",
+    "build_auto_optimizer",
+    "debug",
     "__version__",
 ]
